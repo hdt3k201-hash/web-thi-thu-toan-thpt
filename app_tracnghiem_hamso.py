d1b63a407201c912d68a5fceaf2ff399b102cadd
hdt3k201-hash <h.dt3.k201@gmail.com>
2026-07-31 07:21:04 +0700

    Update app_tracnghiem_hamso.py

--- app_tracnghiem_hamso.py
+++ app_tracnghiem_hamso.py
@@ -79,7 +79,6 @@ st.markdown(
         <span style="color: #008080; font-weight: bold;">Câu 1. </span> 
         <span style="color: #009900; font-weight: bold;">(THPT Lê Thánh Tông HCM 2026) </span>
         Tiệm cận ngang của đồ thị hàm số :
-        
         $y = \dfrac{2x - 3}{x + 1}$ là đường thẳng có phương trình:
     </span>
     """, 
